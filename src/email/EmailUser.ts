49ecf4a980cfd8dccbc93a46c1b6d31bb6c17246
Chua Bing Quan <chipneverdies@gmail.com>
2022-01-07 20:20:10 +0800

    Implement EmailService

---
+++ src/email/EmailUser.ts
@@ -0,0 +1,3 @@
+export default class EmailUser {
+  constructor(public readonly name: string, public readonly address: string) {}
+}
---
+++ src/email/NilLogger.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default class NilLogger {
+  debug(obj: any) {}
+
+  info(obj: any) {}
+
+  warn(obj: any) {}
+
+  error(obj: any) {}
+}
