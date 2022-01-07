8155930a35c297673d3756f830d7b99002777d23
NatalieTanML <nat130613@gmail.com>
2022-01-07 18:19:09 +0800

    Fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ['./src/renderer/**/*.tsx'],
+  content: ['./src/**/*.{js,jsx,tsx}', './src/**/**/*.{js,jsx,tsx}'],
   theme: {},
   plugins: [],
 };
