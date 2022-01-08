5f1640da4e139507d6e290ec7c8cab23f66d0b51
rebecca <e0559622@u.nus.edu>
2022-01-08 11:11:11 +0800

    Kinda broken stitching

--- src/main/main.ts
+++ src/main/main.ts
@@ -8,14 +8,21 @@
  * When running `npm run build` or `npm run build:main`, this file is compiled to
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
+import { match } from 'assert';
 import 'core-js/stable';
+import { Meetings, Preferences, Credential, Meeting } from 'data';
 import { app, ipcMain } from 'electron';
 import log from 'electron-log';
 import { autoUpdater } from 'electron-updater';
+import { UUID } from 'electron-updater/node_modules/builder-util-runtime';
+import EmailService from 'email';
+import Email from 'email/Email';
 import { menubar } from 'menubar';
 import path from 'path';
 import 'regenerator-runtime/runtime';
 import { resolveHtmlPath } from './util';
+const { contextBridge, ipcRenderer } = require('electron');
+
 
 export default class AppUpdater {
   constructor() {
@@ -26,6 +33,70 @@ export default class AppUpdater {
 }
 
 const APPLICATION_DIR = app.getPath('userData');
+
+let inbox: EmailService | null = null;
+const meetings = new Meetings();
+const preferences = new Preferences();
+
+const account = preferences.getCredential();
+if (account != undefined) {
+  inbox = new EmailService(account.email, account.password);
+}
+
+ipcMain.on('auth-login', async (event, credential: Credential) => {
+  if (preferences.getCredential() === undefined) {
+    preferences.updateCredential(credential.email, credential.password);
+    inbox = new EmailService(credential.email, credential.password);
+  }
+  event.returnValue = true;
+
+  if (inbox !== null) {
+    const lastQueriedAt = preferences.getLastQueryDateTime();
+    let emails: Email[] = [];
+    if (preferences.getLastQueryDateTime() === undefined) {
+      const latestSequence = await inbox?.getLatestInboxSequence();
+      const emails = await inbox?.fetchEmailInRange(latestSequence === 0 ? 0 : 1, latestSequence);
+
+    } else {
+      emails = await inbox.fetchEmailSince(lastQueriedAt);
+    }
+
+    preferences.updateLastQueryDateTime(new Date());
+
+    const re = /https:\/\/(.*\.)?zoom.us\/j\/[0-9]+(\?pwd=[a-zA-Z0-9]+)?/;
+    const meetings = window.Electron.meetings.get();
+    const isLoggedIn = window.Electron.auth.login('sumofabiatch45@gmail.com', 'HackAndRoll2022');
+
+    if (isLoggedIn) {
+      for await (let email of emails) {
+        if (email.message?.search(re)) {
+          let url = email.message?.match(re);
+          let urll = "";
+          if (url) {
+            urll = url[0].toString();
+          }
+          meetings.addMeeting(new Meeting(new UUID(email.subject).toString(), email.date, email.subject, email.from, urll));
+        }
+      }
+    }
+
+    event.sender.send("ipc-example", "ping");
+
+    inbox.listenForUpdates(async (email) => {
+      if (isLoggedIn) {
+          if (email.message?.search(re)) {
+            let url = email.message?.match(re);
+            let urll = "";
+            if (url) {
+              urll = url[0].toString();
+            }
+            meetings.addMeeting(new Meeting(new UUID(email.subject).toString(), email.date, email.subject, email.from, urll));
+          }
+      }
+      event.sender.send("ipc-example", "ping");
+    });
+  }
+})
 
 ipcMain.on('ipc-example', async (event, arg) => {
   const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
